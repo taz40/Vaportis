67fd0d8f2183ba2cdda267be593eccde1371e7b9
Esrome <esromethestrange@gmail.com>
2020-02-08 13:57:52 -0700

    Main Menu and Scene Loading
    Added Main Menu.
    Added Loading Screen.

---
+++ Assets/Scripts/Loading/LoadingScene.cs
@@ -0,0 +1,40 @@
+﻿/* Filename: LoadingScene.cs
+ * Author: Caleb
+ */
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/* LoadingScene Class
+ * 
+ * Handles display data of loading bar and loads the next level.
+ */
+public class LoadingScene : MonoBehaviour
+{
+    public Slider ProgressSlider; //The slider to display progress on.
+    public Text ProgressText; //The text to display percent on.
+    [HideInInspector]
+    public static int SceneIndex; //Index of the scene to be loaded. The index refers to its place in the build settings scene list.
+
+    void Start()
+    {
+        //Start a routine without freezing the scene.
+        StartCoroutine(loadAsynchronously());
+    }
+
+    IEnumerator loadAsynchronously()
+    {
+        //Load the scene Asynchronously, allowing us to display data while it loads.
+        AsyncOperation operation = SceneManager.LoadSceneAsync(SceneIndex);
+
+        //Update the slider's progress until loading is complete.
+        while (!operation.isDone)
+        {
+            ProgressSlider.value = operation.progress / 0.9f;
+            ProgressText.text = Mathf.Clamp01(operation.progress / 0.9f) * 100f + "%";
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Loading/SceneLoader.cs
@@ -0,0 +1,45 @@
+﻿/* Filename: SceneLoader.cs
+ * Author: Caleb
+ */
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+/* SceneLoader Class
+ * 
+ * Allows for you to open a new loading screen with a targeted scene to load.
+ */
+public class SceneLoader : MonoBehaviour
+{
+    private const int _LOADING_SCREEN = 1;
+
+    /* Function: LoadScene
+     * 
+     * Args:
+     *  int sceneIndex: the index of the scene to load.
+     *  
+     * Returns: Nothing 
+     * 
+     * Creates a loading screen to load the target scene.
+     */
+    public void LoadScene(int sceneIndex)
+    {
+        //Tell the loading screen which scene to load.
+        LoadingScene.SceneIndex = sceneIndex;
+
+        SceneManager.LoadScene(_LOADING_SCREEN);
+    }
+
+    /* Function: Quit
+     * 
+     * Returns: Nothing
+     * 
+     * Quits the game.
+     */
+    public void Quit()
+    {
+        Application.Quit();
+        EditorApplication.isPlaying = false;
+    }
+}
