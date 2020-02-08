e05cc288d391e348e59b6e43440431d8c7d92afd
Samuel Walker <ldspcjunkie@gmail.com>
2020-02-08 12:53:22 -0700

    Improved the inventory system.
    - Added item based ui colors
    - Auto updating ui
    - scrolling ui
    - dynamic inventory size
    
    Signed-off-by: Samuel Walker <ldspcjunkie@gmail.com>

---
+++ Assets/Scripts/Data/Slot.cs
@@ -0,0 +1,103 @@
+﻿
+/*
+ *
+ * File: Slot.cs
+ * Author: Samuel
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+/*
+ * Slot Class
+ * 
+ * Contains the data for a single inventory slot
+ * Note: This is not a monobehavior, so it does not go on an object.
+ * 
+ */
+public class Slot {
+    private Item _item; //The item in the slot
+    private int _count; //The number of the item in the slot
+    private float _weight; //The weight of item, only here in case we want to make some items weight based, rather than count based i.e. stone
+    private Action<Slot> _onSlotChangedAction;
+
+    //public property for item, calls onSlotChangedAction when modified.
+    public Item Item {
+        get {
+            return _item;
+        }
+
+        set {
+            _item = value;
+            if (_onSlotChangedAction != null)
+                _onSlotChangedAction(this);
+        }
+    }
+
+    //public property for count, calls onSlotChangedAction when modified.
+    public int Count {
+        get {
+            return _count;
+        }
+
+        set {
+            _count = value;
+            if (_onSlotChangedAction != null)
+                _onSlotChangedAction(this);
+        }
+    }
+
+    //public property for weight, calls onSlotChangedAction when modified.
+    public float Weight {
+        get {
+            return _weight;
+        }
+
+        set {
+            _weight = value;
+            if (_onSlotChangedAction != null)
+                _onSlotChangedAction(this);
+        }
+    }
+
+    public Slot() {
+    }
+
+    public Slot(Item item, int count = 1, int weight = 0){
+        _item = item;
+        _count = count;
+        _weight = weight;
+    }
+
+    //---------------Callback Functions---------------------
+
+    /*
+     * Function: RegisterOnSlotModified
+     * Params: 
+     *   cb: the callback to register
+     * Returns: None
+     * 
+     * Registers cb to the callback, so it will fire.
+     * 
+     */
+    public void RegisterOnSlotModified(Action<Slot> cb) {
+        _onSlotChangedAction += cb;
+    }
+
+    /*
+     * Function: UnregisterOnSlotModified
+     * Params: 
+     *   cb: the callback to unregister
+     * Returns: None
+     * 
+     * Unregisters cb from the callback, so it will not fire.
+     * 
+     */
+    public void UnregisterOnSlotModified(Action<Slot> cb) {
+        _onSlotChangedAction -= cb;
+    }
+
+}
--- Assets/Scripts/UI/InventoryController.cs
+++ Assets/Scripts/UI/InventoryController.cs
@@ -8,6 +8,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /*
  * Inventory Controller Class
@@ -21,10 +22,10 @@ public class InventoryController : MonoBehaviour{
     public GameObject SlotGameObject; //Prefab for slot gameobject
     public GameObject InventoryPanel; //The inventory's panel object
     public GameObject InventoryObject; //The root inventory object in the heirarchy
-    public int InitialInventorySize = 30; //The initial size of the inventory
 
     private bool _isInventoryOpen = false;//True if inventory is open, false otherwise
     private Inventory _inventory; //The inventory data
+    private Dictionary<Slot, GameObject> _slotDictionary; //A dictionary to keep track of all the slots linked to the game objects
 
     /*
      * Function: InitializeInventoryUI
@@ -66,6 +67,16 @@ public class InventoryController : MonoBehaviour{
             }
             _isInventoryOpen = !_isInventoryOpen;
         }
+
+
+        //TEMPORARY
+        if (Input.GetButtonDown("Add")) {
+            ItemType type = (ItemType)Random.Range(0, 3);
+            _inventory.Add(new Slot(new Item(type)));
+        }
+        if (Input.GetButtonDown("Remove")) {
+            _inventory.Remove(_inventory.Size-1);
+        }
     }
 
     private void openInventory() {
@@ -85,12 +96,118 @@ public class InventoryController : MonoBehaviour{
      * Params: None
      * Returns: Void
      * 
-     * Unity build in function. Will run on the start of the game, just initializes some values.
+     * Unity built in function. Will run on the start of the game, just initializes some values.
      * 
      */
     public void Start() {
-        _inventory = new Inventory(InitialInventorySize);
-        InitializeInventoryUI(_inventory.Size);
+        _inventory = new Inventory();
+        _slotDictionary = new Dictionary<Slot, GameObject>();
+
+        //register callbacks
+        _inventory.RegisterOnSlotAdded(OnSlotAdded);
+        _inventory.RegisterOnSlotRemoved(OnSlotRemoved);
+        ItemType type = (ItemType)Random.Range(0, 3);
+        _inventory.Add(new Slot(new Item(type)));
+        //InitializeInventoryUI(_inventory.Size);
+    }
+
+    //--------------Callbacks-------------------
+
+    /*
+     * Function: OnSlotAdded
+     * Params: 
+     *   Slot: the slot that was added
+     * Returns: Void
+     * 
+     * Callback that is called when a new slot is added. This will handle updating the UI
+     * 
+     */
+    public void OnSlotAdded(Slot slot) {
+        GameObject go = Instantiate(SlotGameObject);
+        go.transform.SetParent(InventoryPanel.transform);
+        Image im = go.GetComponent<Image>();
+        Color color = Color.white;
+        switch (slot.Item.Type) {
+            case ItemType.Red:
+                color = Color.red;
+                break;
+            case ItemType.Blue:
+                color = Color.blue;
+                break;
+            case ItemType.Green:
+                color = Color.green;
+                break;
+        }
+        im.color = color;
+        go.GetComponent<Button>().onClick.AddListener(() => { OnSlotClick(slot); });
+        _slotDictionary.Add(slot, go);
+        slot.RegisterOnSlotModified(OnSlotModified);
+    }
+
+    /*
+     * Function: OnSlotRemoved
+     * Params: 
+     *   Slot: the slot that was removed
+     * Returns: Void
+     * 
+     * Callback that is called when a slot is removed. This will handle updating the UI
+     * 
+     */
+    public void OnSlotRemoved(Slot slot) {
+        Destroy(_slotDictionary[slot]);
+        _slotDictionary.Remove(slot);
+    }
+
+    /*
+     * Function: OnSlotModified
+     * Params: 
+     *   Slot: the slot that was modified
+     * Returns: Void
+     * 
+     * Callback that is called when a slot is modified. This will handle updating the UI
+     * 
+     */
+    public void OnSlotModified(Slot slot) {
+        GameObject go = _slotDictionary[slot];
+        Image im = go.GetComponent<Image>();
+        Color color = Color.white;
+        switch (slot.Item.Type) {
+            case ItemType.Red:
+                color = Color.red;
+                break;
+            case ItemType.Blue:
+                color = Color.blue;
+                break;
+            case ItemType.Green:
+                color = Color.green;
+                break;
+        }
+        im.color = color;
+    }
+
+    /*
+     * Function: OnSlotClick
+     * Params: 
+     *   Slot: the slot that was clicked
+     * Returns: Void
+     * 
+     * Callback that is called when a slot is clicked. This will handle updating the inventory.
+     * 
+     */
+    public void OnSlotClick(Slot slot) {
+        ItemType type = ItemType.Red;
+        switch (slot.Item.Type) {
+            case ItemType.Red:
+                type = ItemType.Blue;
+                break;
+            case ItemType.Blue:
+                type = ItemType.Green;
+                break;
+            case ItemType.Green:
+                type = ItemType.Red;
+                break;
+        }
+        slot.Item = new Item(type);
     }
 
 }
