f7a75089ee7a8a50bf9eec7bddf6a8046f4c252e
Esrome <esromethestrange@gmail.com>
2020-02-08 18:04:01 -0700

    Options Started

---
+++ Assets/Scripts/Data/Settings.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings: MonoBehaviour
+{
+    public Dropdown GraphicsDropdown;
+    public Toggle FullscreenToggle;
+
+    public void Start()
+    {
+        GraphicsDropdown.value = QualitySettings.GetQualityLevel();
+        FullscreenToggle.isOn = Screen.fullScreen;
+    }
+
+    public void SetQuality(int quality)
+    {
+        QualitySettings.SetQualityLevel(quality);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
